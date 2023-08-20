276263ad6ac2f25ef5df9cd1b511f5556867adf8
asebeyakob@gmail.com <asebeyakob@gmail.com>
2023-08-20 09:30:12 -0600

    minipro

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,13 @@
 import React from "react";
+import { CATEGORIES } from "../data"; // Make sure to import CATEGORIES
 
 function CategoryFilter() {
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {CATEGORIES.map((category) => (
+        <button key={category}>{category}</button>
+      ))}
     </div>
   );
 }
